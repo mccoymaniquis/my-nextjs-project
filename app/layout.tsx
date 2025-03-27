49680b665b7e1edb3c0f09cc10d79359570765aa
Louis Mccoy Maniquis <54703326+mccoymaniquis@users.noreply.github.com>
2025-03-28 07:09:18 +0800

    New branch
    * lint-staged-setup
    
    * test if this should have error
    
    * this should pass
    
    * adding shadCN and disable vscode in git ignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import React from "react";
 
 import "./globals.css";
 
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,7 +33,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarTrigger />
+          <main>
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
