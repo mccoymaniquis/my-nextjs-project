2b1ea2d7fafaee8a59131061feacc0eb765a079a
mccoymaniquis <louismaniquis@gmail.com>
2025-03-28 11:09:23 +0800

    sidebar navigation

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,4 +1,19 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Building,
+  CalendarCheck,
+  CalendarCheck2,
+  CalendarClock,
+  FileUser,
+  HandCoins,
+  Home,
+  List,
+  ListCheck,
+  Mailbox,
+  ReceiptText,
+  Settings,
+  SquareUserRound,
+  UserPen,
+} from "lucide-react";
 
 import {
   Sidebar,
@@ -12,43 +27,126 @@ import {
 } from "@/components/ui/sidebar";
 
 // Menu items.
-const items = [
+const hrItems = [
+  {
+    title: "Employee List",
+    url: "#",
+    icon: List,
+  },
+  {
+    title: "Request",
+    url: "#",
+    icon: Mailbox,
+  },
+  {
+    title: "Timekeeping",
+    url: "#",
+    icon: CalendarClock,
+  },
+  {
+    title: "Payroll",
+    url: "#",
+    icon: HandCoins,
+  },
+  {
+    title: "shift schedule",
+    url: "#",
+    icon: CalendarCheck,
+  },
+  {
+    title: "Job Structure",
+    url: "#",
+    icon: Building,
+  },
+];
+const employeeItems = [
   {
     title: "Home",
     url: "#",
     icon: Home,
   },
   {
-    title: "Inbox",
+    title: "Schedule",
     url: "#",
-    icon: Inbox,
+    icon: CalendarCheck2,
   },
   {
-    title: "Calendar",
+    title: "Personal Details",
     url: "#",
-    icon: Calendar,
+    icon: UserPen,
   },
   {
-    title: "Search",
+    title: "Payslip",
     url: "#",
-    icon: Search,
+    icon: ReceiptText,
   },
+  {
+    title: "Attendance",
+    url: "#",
+    icon: ListCheck,
+  },
+  {
+    title: "Leave Request",
+    url: "#",
+    icon: FileUser,
+  },
+];
+const adminItems = [
   {
     title: "Settings",
     url: "#",
     icon: Settings,
   },
+  {
+    title: "User Management",
+    url: "#",
+    icon: SquareUserRound,
+  },
 ];
 
 export function AppSidebar() {
   return (
-    <Sidebar>
+    <Sidebar variant="sidebar">
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>HR</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map(item => (
+              {hrItems.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Employee</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {employeeItems.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Administrator</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {adminItems.map(item => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
                     <a href={item.url}>
